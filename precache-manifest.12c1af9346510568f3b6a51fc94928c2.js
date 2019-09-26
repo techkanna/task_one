cef5fcda3f54d0ecb0901c186b84667ea26fab79
Senthil Kannan <senthilkannanarmy@gmail.com>
2019-09-26 17:13:58 +0530

    Updates

---
+++ precache-manifest.12c1af9346510568f3b6a51fc94928c2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f624fc7d0c98c4ce53d8a7af1ebc34e0",
+    "url": "/task_one/index.html"
+  },
+  {
+    "revision": "c5b67b3204dfde6efc2c",
+    "url": "/task_one/static/css/main.7db8386e.chunk.css"
+  },
+  {
+    "revision": "07dc2420cf8f68047d72",
+    "url": "/task_one/static/js/2.5261d7bc.chunk.js"
+  },
+  {
+    "revision": "c5b67b3204dfde6efc2c",
+    "url": "/task_one/static/js/main.37982ced.chunk.js"
+  },
+  {
+    "revision": "ed0302edcfa69c72c1b8",
+    "url": "/task_one/static/js/runtime-main.37323877.js"
+  }
+]);
